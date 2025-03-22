76ca1c954223ed3a7c871acf296d2b110d2d5403
Md Willowy <mdwillowy@MDs-MacBook-Air.local>
2025-03-22 20:41:56 +0530

    changes in vite.config.js

--- task-ey-gds/vite.config.js
+++ task-ey-gds/vite.config.js
@@ -1,14 +1,14 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  plugins: [react()],
+  base: '/ey-gds-task1/', // Set the base path for GitHub Pages
   build: {
-    outDir: 'dist',
+    outDir: 'dist', // Output directory for the build
   },
-  serve: {
-    port: 5173,
-    host: 'localhost',
-  },
-  public: {
-    directory: 'public',
+  server: {
+    port: 5173, // Port for the development server
+    host: 'localhost', // Host for the development server
   },
 });
